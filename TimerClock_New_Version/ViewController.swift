490b2518302e21520b729e997ae2ebb1fdc7a3c0
Diego Souza Sampaio <digdig@MacBook-Air-de-Diego.local>
2017-02-15 13:35:40 -0500

    Initial Commit

---
+++ TimerClock_New_Version/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimerClock_New_Version
+//
+//  Created by Diego Souza Sampaio on 17-02-15.
+//  Copyright © 2017 Diego Souza Sampaio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
